62ec6d2547847195bad89c971b0ae2b9877e0d64
Gabriel Packer <ggkpacker@gmail.com>
2018-10-17 14:06:36 -0300

    Refactor slices

--- series/series.rb
+++ series/series.rb
@@ -1,22 +1,11 @@
-require "byebug"
 class Series
-  attr_reader :series
-
-  def initialize(series)
-    @series = series
+  def initialize(digits)
+    @digits = digits
   end
 
   def slices(slice_size)
-    # TODO: refactor
-    raise ArgumentError.new('asds') if slice_size > series.size
-    a = series.chars
-    b = series.chars
-    result = []
-    a.each do |item|
-      slice = b.first(slice_size).join
-      result << slice if slice.size == slice_size
-      b.shift
-    end
-    result
+    raise ArgumentError if slice_size > @digits.size
+
+    (0..@digits.length - slice_size).map { |index| @digits[index, slice_size] }
   end
 end
