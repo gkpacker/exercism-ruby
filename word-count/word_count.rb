4bbe63690d8351fff974b043f906ec2f8502d812
Gabriel Packer <ggkpacker@gmail.com>
2018-10-17 15:58:06 -0300

    Refactor with improved regex

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,14 +1,15 @@
 class Phrase
-  def initialize(word)
-    @word = word
+  def initialize(words)
+    @words = from_phrase(words)
   end
 
   def word_count
-    frequency = Hash.new(0)
-    @word.split(/(\w+'\w+)?(\w+)?(^\s)/)
-         .map(&:strip)
-         .reject(&:empty?)
-         .each { |word| frequency[word.downcase] += 1 }
-    frequency
+    @words.each_with_object(Hash.new(0)) { |w, h| h[w] += 1 }
+  end
+
+  private
+
+  def from_phrase(phrase)
+    phrase.scan(/\b[^\s,]+\b/).each { |w| w.downcase! }
   end
 end
